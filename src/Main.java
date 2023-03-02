dc94bb903060dceca142ed82c00949085fdf654d
dinarfathrahmanov <dinfat@mail.ru>
2023-03-02 22:23:08 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        /*task2();
+        task3();
+        task4();*/
     }
-}
+    public static int years(int year){
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+            return year;
+            }
+    public static int task1 () {
+        System.out.println("Задача 1");
+        return years(1995);
+        }
+    }
+
